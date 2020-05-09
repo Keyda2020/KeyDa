0a73c14c3133241937358ed34c3770f98e0bb4c9
anthonyminyungi <dbstnsdl12@naver.com>
2020-05-09 20:07:00 +0900

    add husky hooks and shell script for branch checking

---
+++ scripts/check-protected-branch.sh
@@ -0,0 +1,10 @@
+CURRENT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+
+if [ $CURRENT_BRANCH_NAME = "develop" ] || [ $CURRENT_BRANCH_NAME = "master"];
+  then
+    echo "committing or pushing is prevented in branch [$CURRENT_BRANCH_NAME].";
+    exit 1; #Unsuccessful
+else
+  echo "successfully commited.";
+  exit 0; #Successful
+fi
