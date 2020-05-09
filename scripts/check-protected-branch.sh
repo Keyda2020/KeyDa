f75606aafc608f0e5cac24f4819c672556290bd3
anthonyminyungi <dbstnsdl12@naver.com>
2020-05-09 20:18:12 +0900

    fix shell script error

--- scripts/check-protected-branch.sh
+++ scripts/check-protected-branch.sh
@@ -1,6 +1,6 @@
 CURRENT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 
-if [ $CURRENT_BRANCH_NAME = "develop" ] || [ $CURRENT_BRANCH_NAME = "master"];
+if [ $CURRENT_BRANCH_NAME = "develop" ] || [ $CURRENT_BRANCH_NAME = "master" ];
   then
     echo "committing or pushing is prevented in branch [$CURRENT_BRANCH_NAME].";
     exit 1; #Unsuccessful
