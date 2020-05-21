aedfc3945fd2db5d2a04c8b88ba95d4d90da0cd7
anthonymin <dbstnsdl12@naver.com>
2020-05-21 15:27:24 +0900

    Set up python shell usage

--- packages/keyda-server/src/routes/users.js
+++ packages/keyda-server/src/routes/users.js
@@ -1,11 +1,43 @@
 const express = require('express');
+const { PythonShell } = require('python-shell');
+const config = require('../config');
+const options = {
+  mode: 'text',
+  pythonPath: '',
+  pythonOptions: ['-u'],
+  scriptPath: '',
+  args: [],
+};
+
 const router = express.Router();
 
-router.post('/register', (req, res) => {
-  console.log(req.headers.origin);
-  console.log(req.body);
+router.post('/register', async (req, res) => {
+  const keyTimeList = req.body.keyTimeList;
+  const userId = req.body.userId;
+
+  if (keyTimeList === [] || userId === '') {
+    return res.status(204).send({
+      success: false,
+      error: true,
+      message: 'You missed some input, so you can`t register.',
+    });
+  }
+
+  const reqOrigin = req.headers.origin;
+  const data = req.body;
+
+  const resultFromModel = await new Promise((resolve, reject) => {
+    const shell = PythonShell.run(config.MODEL_PATH, null, (err, results) => {
+      if (err) return reject(err);
+      return resolve(results);
+    });
+  });
+
   return res.status(200).send({
     success: true,
+    error: false,
+    message: 'good',
+    pyData: resultFromModel,
   });
 });
 
