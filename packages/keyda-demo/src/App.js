e0f98878da84ac27ccb84a8e161ccb724a2c36d7
anthonymin <dbstnsdl12@naver.com>
2020-05-23 23:23:30 +0900

    Add log-in form to demo package

--- packages/keyda-demo/src/App.js
+++ packages/keyda-demo/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import RegisterForm from './RegisterForm';
+import LoginForm from './LoginForm';
 
 function App() {
   return (
     <div className="App">
+      <h3>Register Form</h3>
       <RegisterForm />
+      <br />
+      <hr />
+      <h3>Login Form</h3>
+      <LoginForm />
     </div>
   );
 }
